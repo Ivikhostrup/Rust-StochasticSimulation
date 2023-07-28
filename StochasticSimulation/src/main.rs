c61306bcf0b725f648cf06f13ea8e2fa486c546a
Ivikhostrup <ihostr16@student.aau.dk>
2023-07-28 09:54:17 +0200

    Added some code for visitor pattern

--- StochasticSimulation/src/main.rs
+++ StochasticSimulation/src/main.rs
@@ -12,17 +12,36 @@ pub trait Visitor<'a> {
 
 pub struct SystemVisitor;
 
-//impl<'a> Visitor<'a> for SystemVisitor {
+impl<'a> Visitor<'a> for SystemVisitor {
+    fn visit_system(&mut self, system: &Arc<Mutex<ChemicalSystem>>) {
+        let system = system.lock().unwrap();
 
-//}
+        for reaction in &system.reactions {
+            self.visit_reactions(reaction);
+        }
+    }
+
+    fn visit_reactions(&mut self, reaction: &Arc<Mutex<Reaction>>) {
+        let reaction = reaction.lock().unwrap();
+        let min_delay = 1;
+
+        
+    }
+
+    fn visit_species(&mut self, species: &Arc<Mutex<Species>>) {
+        todo!()
+    }
+}
 
 pub struct ChemicalSystem {
-    reactions: Vec<Arc<Mutex<Reaction>>>
+    reactions: Vec<Arc<Mutex<Reaction>>>,
+    min_delay: Option<f64>,
+    reaction_with_min_delay: Option<Arc<Mutex<f64>>>
 }
 
 impl ChemicalSystem {
     fn new(reactions: Vec<Arc<Mutex<Reaction>>>) -> Arc<Mutex<ChemicalSystem>> {
-        Arc::new(Mutex::new(Self {reactions}))
+        Arc::new(Mutex::new(Self {reactions, min_delay: None}))
     }
 
     fn accept(system: Arc<Mutex<Self>>, visitor: &mut dyn Visitor) {
@@ -45,8 +64,12 @@ impl Reaction {
         Arc::new(Mutex::new(Reaction { reactants, products, delay, lambda}))
     }
 
-    fn accept(&mut self, visitor: &mut dyn Visitor) {
-        visitor.visit_reactions(self);
+    fn accept(reaction: Arc<Mutex<Reaction>>, visitor: &mut dyn Visitor) {
+        visitor.visit_reactions(&reaction);
+    }
+
+    fn compute_delay() {
+
     }
 }
 
@@ -56,8 +79,12 @@ pub struct Species {
 }
 
 impl Species {
-    fn accept(&self, visitor: &mut dyn Visitor) {
-        visitor.visit_species(Arc::clone(&self));
+    fn new(name: String, quantity: i32) -> Arc<Mutex<Species>> {
+        Arc::new(Mutex::new(Species {name, quantity}))
+    }
+
+    fn accept(species: Arc<Mutex<Species>>, visitor: &mut dyn Visitor) {
+        visitor.visit_species(&species);
     }
 }
 
